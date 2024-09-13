42f953d41e00e3056f05e50a3a2db6c5f035ec04
ImPanyAngel <angelides.pany04@gmail.com>
2024-09-13 20:32:35 +0400

    test fixing images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import Navbar from "../components/NavBar";
 import Project from "../components/Project";
 
+import moneyManagerIntro from './assets/money-manager-intro.png';
+
 function Home(){
     
     return(
         <>
         <Navbar/>
         <h1 className="home-title">Hello, I&rsquo;m Pany. Student at University of Leicester, with a passion for creating, learning, and exploring new ideas.</h1>
-        <Project name={"MoneyManager"} image={"./src/assets/money-manager-intro.png"}/>
+        <Project name={"MoneyManager"} image={moneyManagerIntro}/>
         </>
     );
 }
